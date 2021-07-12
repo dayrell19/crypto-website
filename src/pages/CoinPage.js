61a238b3b22d554301a5a238b5b73f740e2cbd7b
dayrell19 <leonardodayrell.reis@gmail.com>
2021-07-12 16:20:04 -0400

    Finalized Project

--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -7,7 +7,7 @@ import Axios from "axios";
 function CoinPage() {
   let { id } = useParams();
   let history = useHistory();
-  const [coinInfo, setCoinInfo] = useState();
+  const [coinInfo, setCoinInfo] = useState(null);
 
   useEffect(() => {
     Axios.get(`https://api.coingecko.com/api/v3/coins/${id}`)
@@ -18,14 +18,82 @@ function CoinPage() {
       .catch(() => {
         history.push("/pagenotfound");
       });
-  }, []);
+  }, [history, id]);
 
-  return (
-    <div>
-      <h1>Coin Page Bitch</h1>
-      <h3>Coin chosen: {id}</h3>
-    </div>
-  );
+  if (coinInfo) {
+    return (
+      <div className="coinPageContainer">
+        <div className="wrap">
+          <h1 className="coinPageName">{coinInfo.name}</h1>
+
+          <img
+            src={coinInfo.image.large}
+            className="coinPageLogo"
+            alt="que buhao"
+          />
+
+          <div className="row">
+            <p className="coinPagePrice">
+              Price: ${coinInfo.market_data.current_price.usd}
+            </p>
+            <p
+              className="coinPagePercentage"
+              id={
+                coinInfo.market_data.market_cap_change_percentage_24h < 0
+                  ? "redPercentage"
+                  : "greenPercentage"
+              }
+            >
+              {coinInfo.market_data.market_cap_change_percentage_24h}%
+            </p>
+            <p
+              className="coinPagePriceChange"
+              id={
+                coinInfo.market_data.price_change_24h_in_currency.usd < 0
+                  ? "redPrice"
+                  : "greenPrice"
+              }
+            >
+              $
+              {coinInfo.market_data.price_change_24h_in_currency.usd.toFixed(5)}
+            </p>
+          </div>
+          <div className="row">
+            <p className="coinPageLow">
+              Daily Low: ${coinInfo.market_data.low_24h.usd}
+            </p>
+            <p className="coinPageHigh">
+              Daily High: ${coinInfo.market_data.high_24h.usd}
+            </p>
+          </div>
+          <div className="row">
+            <p className="coinPageCap">
+              Market Cap: $
+              {coinInfo.market_data.market_cap.usd.toLocaleString()}
+            </p>
+          </div>
+          <div className="secondrow">
+            <p className="coinPageSupply">
+              Circulating Supply: {coinInfo.market_data.circulating_supply}/
+              {coinInfo.market_data.max_supply == null
+                ? "undetermined"
+                : coinInfo.market_data.max_supply}
+            </p>
+          </div>
+          <button
+            class="goBackButton"
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            Back
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default CoinPage;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -16,7 +16,6 @@ function Main() {
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false"
     ).then((response) => {
       setCryptoList(response.data);
-      console.log(response.data);
     });
   }, []);
 
